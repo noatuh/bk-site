d11d640f8496135f0c21e4be4ce7500d1f80a16f
noatuh <noatuh@outlook.com>
2023-09-05 21:54:40 -0400

    added order fetching and reception page

--- server.js
+++ server.js
@@ -1,33 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');  // Required for setting up the static files path
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware to parse JSON data from incoming requests
 app.use(bodyParser.json());
 
+// Enable CORS for all routes
+app.use(cors());
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+let orders = [];
+
+// Endpoint to serve the main page (index.html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Endpoint to serve the order view page (orderView.html)
+app.get('/view-orders', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'orderView.html'));
+});
+
 // Endpoint to receive data from the client
 app.post('/receive-data', (req, res) => {
-    console.log("Received data:", req.body);  // This will print the received data to the terminal
+    console.log("Received data:", req.body);
     res.status(200).send("Data received successfully!");
+});
+
+// Endpoint to receive order from the client
+app.post('/submit-order', (req, res) => {
+    const order = req.body;
+    console.log("Received order:", order);
+    orders.push(order);
+    res.json({ message: 'Order received!' });
+});
+
+// Endpoint to get all orders
+app.get('/get-orders', (req, res) => {
+    res.json(orders);
 });
 
 // Start the server on port 8080
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
-});
-const cors = require('cors');
-app.use(cors());
-
-app.post('/submit-order', (req, res) => {
-    const order = req.body;
-    console.log("Received order:", order);
-
-    // Here, you can process the order, save it to a database, etc.
-
-    res.json({ message: 'Order received!' });
 });
